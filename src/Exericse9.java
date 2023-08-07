5d7b8c6f7ec1eda9bc189fceddda82f5000e1225
Dong <thanhdong200425@gmail.com>
2023-08-07 10:32:31 +0700

    .

--- src/Exericse9.java
+++ src/Exericse9.java
@@ -40,19 +40,51 @@ public class Exericse9 {
         int count = 0;
         for (int i = 0; i < length; i++) {
             if (arr[i] > 0) {
-                result*=arr[i];
+                result *= arr[i];
                 count++;
             }
         }
-        if(count == 0) {
+        if (count == 0) {
             System.out.println("This array hasn't positive number!");
         }
         return result;
     }
 
+    static void swap(int[] arr, int one, int two) {
+        int temp = arr[one];
+        arr[one] = arr[two];
+        arr[two] = temp;
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int i = low - 1;
+        int pivot = arr[high];
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return (i + 1);
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+
     public static void main(String[] args) {
         Exericse9 e = new Exericse9();
-        int[] array = e.createArray(11);
+        int[] array = e.createArray(10);
 
         Exercise3.print(array);
         System.out.println("--------------");
@@ -60,7 +92,9 @@ public class Exericse9 {
         e.reverseArray(array);
         Exercise3.print(array);
 
-        System.out.println(e.sumOfPositiveNumber(array));
-        System.out.println(e.multiplyOfPositiveNumber(array));
+        System.out.println("---------------");
+
+        Exericse9.quickSort(array, 0, array.length - 1);
+        Exercise3.print(array);
     }
 }
